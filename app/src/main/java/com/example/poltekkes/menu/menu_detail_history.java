3465b8d1020a1521678a0a4098d5e8d0cf9b759c
rdadesukro <rdade30@gmail.com>
2021-09-24 13:59:23 +0700

    DARI LEPTOP

--- app/src/main/java/com/example/poltekkes/menu/menu_detail_history.java
+++ app/src/main/java/com/example/poltekkes/menu/menu_detail_history.java
@@ -1,9 +1,13 @@
 package com.example.poltekkes.menu;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -11,7 +15,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.poltekkes.R;
 import com.example.poltekkes.adapter.adapter_detail_history;
-import com.example.poltekkes.adapter.adapter_history;
 import com.example.poltekkes.model.detail_history.JawabanItem;
 import com.example.poltekkes.presenter.history_detail;
 import com.example.poltekkes.view.history_detail_view;
@@ -24,14 +27,31 @@ public class menu_detail_history extends AppCompatActivity implements history_de
     private androidx.recyclerview.widget.RecyclerView RecyclerView;
     private ProgressBar progressBar;
     com.example.poltekkes.presenter.history_detail history_detail;
-    String id;
+    String id, nama, tgl_lahir, alamat, bb, pb, nama_ibu, jenis_kelamin;
+    public String data;
+    private TextView webDataBayi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_detail_history);
         initView();
+        data = "<h2>Pengenalan Aplikasi</h2>\n" +
+                "<ul>\n" +
+                "    <li>Nama Balita         : "+ nama +"</li>"+
+                "    <li>Tangal Lahir        : "+ tgl_lahir +"</li>"+
+                "    <li>Jenis Kelamin : " + jenis_kelamin +"</li>"+
+                "    <li>Berat Badan      : "+ bb +"</li>"+
+                "    <li>Panjang Badan   : "+ pb +"</li>"+
+                "    <li>Alamat          :</li>" + alamat +
+                "    <li>Nama Ibu        :</li>" + nama_ibu +
+                "</ul>";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            webDataBayi.setText(Html.fromHtml(data, Html.FROM_HTML_MODE_LEGACY));
+        } else
+            webDataBayi.setText(Html.fromHtml(data));
         id = Guru.getString("id_detail_histiry", "false");
-        history_detail = new history_detail(this,menu_detail_history.this);
+        history_detail = new history_detail(this, menu_detail_history.this);
         history_detail.get_history_detail(id);
     }
 
@@ -40,7 +60,7 @@ public class menu_detail_history extends AppCompatActivity implements history_de
         try {
             Log.i("data_history", "history: " + history_detail.size());
 
-            adapter_detail_history = new adapter_detail_history(menu_detail_history.this, history_detail, 1,null);
+            adapter_detail_history = new adapter_detail_history(menu_detail_history.this, history_detail, 1, null);
             RecyclerView.setLayoutManager(new LinearLayoutManager(menu_detail_history.this, LinearLayoutManager.VERTICAL, false));
             RecyclerView.setHasFixedSize(true);
             adapter_detail_history.notifyDataSetChanged();
@@ -55,7 +75,7 @@ public class menu_detail_history extends AppCompatActivity implements history_de
 
             }
         } catch (Exception e) {
-            Log.i("cek_data_history", "history: "+e.getMessage());
+            Log.i("cek_data_history", "history: " + e.getMessage());
 
         }
     }
@@ -63,5 +83,6 @@ public class menu_detail_history extends AppCompatActivity implements history_de
     private void initView() {
         RecyclerView = findViewById(R.id.RecyclerView);
         progressBar = findViewById(R.id.progressBar);
+        webDataBayi = (TextView) findViewById(R.id.web_data_bayi);
     }
 }
